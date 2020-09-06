238ed34ae905ace4b44bd0892fad4c86abf15ceb
tamimvaiya <tamimvaiya.123@gmail.com>
2020-09-06 13:51:22 +0600

    added many things

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -18,7 +18,10 @@ const Product = (props) => {
         <p><small>by: {seller}</small></p>
         <p>${price}</p>
         <p><small>Only {stock} left in stock - Order soon</small></p>
-        <button className='main-button'><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+        <button
+          onClick={() => props.handleAddProduct(props.product)}
+          className='main-button'
+        ><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
       </div>
     </div>
 
