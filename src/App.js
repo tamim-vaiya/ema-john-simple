a46e8a4fcde21911111ae99f9a2cce679e9d07a1
tamimvaiya <tamimvaiya.123@gmail.com>
2020-09-09 17:16:50 +0600

    added few things

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
-      
+      <Router>
+
+        <Switch>
+
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+
+          <Route path="/product/:productKey">
+            <ProductDetail></ProductDetail>
+          </Route>
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -22,6 +22,7 @@ const Shop = () => {
       <div className="product-container">
         {
           products.map(pd => <Product
+            showAddToCart={true}
              handleAddProduct = {handleAddProduct} 
              product={pd}
              ></Product>)
